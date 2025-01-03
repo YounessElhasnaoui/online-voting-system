5bd48e7b31de2130972bc81a42ead90748a6d46f
YounessElhasnaoui <youness.elhasnaoui1@usmba.ac.ma>
2025-01-03 14:58:30 +0100

    Initialize backend with Express server

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: false, // Deprecated in newer Mongoose versions
+      // useCreateIndex: true, // Deprecated in newer Mongoose versions
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;

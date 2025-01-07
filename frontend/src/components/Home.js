54de1437d8f0b25ab2db733f9c7c71e0a191cc95
Lbertouch123@gmail.com <Achrafrhd123@gmail.com>
2025-01-07 15:20:22 +0100

    add: login page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Welcome to Home Page</h1>
+      <p>You are logged in!</p>
+    </div>
+  );
+};
+
+export default Home;
